d56f475fef23073cf8e94e6b4bfef213f2104304
manojfst88 <121332264+manojfst88@users.noreply.github.com>
2025-08-15 19:33:26 +0530

    New files

---
+++ src/main/java/com/ims/customer/ImsCustomerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ims.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImsCustomerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImsCustomerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ims/customer/ImsCustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ims.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImsCustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
